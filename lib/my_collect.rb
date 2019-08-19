86d39c464607cfec0f40630beba92b723cf31c07
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-19 00:44:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0 
+  caller_location
+  while i < array.length 
+    caller_location << yield(array[i])
+    i += 1
+  end 
+  caller_location
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first 
+end
